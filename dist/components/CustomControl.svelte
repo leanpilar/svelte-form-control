681706e5c4ac0607d29ef641550a7f67f029bfa4
Lean <lean.pilar@infosit.com>
2022-08-16 09:24:48 +0200

    meta and custom control

---
+++ dist/components/CustomControl.svelte
@@ -0,0 +1,58 @@
+<script>
+  import {ControlError} from "./index";
+  import {
+    controlClasses,
+  } from '@leanpilar/svelte-form-control';
+  export let control;
+  export let label = '';
+  let state = control.state
+  let value = control.value
+  let controls = control.controls;
+  function addControl() {
+    if ($state?.$meta?.emptyControl) {
+      control.pushControl($state.$meta.emptyControl())
+    }
+  }
+</script>
+
+{#if control}
+  {#if $state.$type === 'control'}
+    <label>
+      <span class="label">{$state?.$meta?.name || label}</span>
+      <input placeholder="{$state?.$meta?.placeholder}" bind:value={$value} use:controlClasses={control} />
+      <ControlError control={control}/>
+    </label>
+  {/if}
+  {#if $state.$type === 'group'}
+    <fieldset>
+      {#each Object.entries($controls) as [label, control]}
+        <svelte:self {control} {label}>
+        </svelte:self>
+      {/each}
+    </fieldset>
+  {/if}
+  {#if $state.$type === 'array'}
+    <fieldset>
+      {#each $controls as ctrl, i}
+        <fieldset>
+        <svelte:self control="{ctrl}" >
+        </svelte:self>
+        <button on:click={control.removeControlAt(i)}>remove me</button>
+        </fieldset>
+      {/each}
+      <button on:click={addControl}>add one</button>
+    </fieldset>
+  {/if}
+{/if}
+
+
+<style>
+  label .label {
+    display: block;
+  }
+
+  fieldset {
+    border: none;
+    margin-top: 1em;
+  }
+</style>
---
+++ dist/components/CustomInput.svelte
@@ -0,0 +1,20 @@
+<script>
+  import {ControlError} from "./index";
+  import {
+    controlClasses,
+  } from '@leanpilar/svelte-form-control';
+  export let control;
+  let state = control.state
+  let value = control.value
+  console.log($value);
+</script>
+
+{#if control}
+  {#if $state.$type === 'control'}
+    <label>
+      <span class="label">email:</span>
+      <input bind:value={$value.email} use:controlClasses={control} />
+      <ControlError control={control}/>
+    </label>
+  {/if}
+{/if}
