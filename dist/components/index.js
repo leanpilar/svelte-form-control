87f286aad13eb43a68990f6ddc07a7b2c8187548
Lean <lean.pilar@infosit.com>
2022-08-16 09:49:36 +0200

    wrong export temp fix

--- dist/components/index.js
+++ dist/components/index.js
@@ -1,2 +1,2 @@
-export { default as ControlError, defaultErrorMessages } from './ControlError.svelte';
+export { default as ControlError } from './ControlError.svelte';
 export {default as CustomControl} from './CustomControl.svelte';
