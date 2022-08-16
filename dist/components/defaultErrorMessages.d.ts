18de2ccc82bdf59c1d8c8227773a941a0de924e1
Lean <lean.pilar@infosit.com>
2022-08-16 09:54:08 +0200

    wrong export

---
+++ dist/components/defaultErrorMessages.d.ts
@@ -0,0 +1,12 @@
+export declare const defaultErrorMessages: {
+    required: string;
+    email: string;
+    minLength: (min: any) => string;
+    maxLength: (max: any) => string;
+    number: string;
+    decimal: string;
+    integer: string;
+    min: (min: any) => string;
+    max: (max: any) => string;
+    pattern: (pattern: any) => string;
+};
